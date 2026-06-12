37afc1cf2373b1d66fa716f3f4e5a9d0466c743e
ChaoticChrono <ChaoticChrono@proton.me>
2026-06-12 21:25:45 +0530

    Added gamescope

--- configuration.nix
+++ configuration.nix
@@ -327,7 +327,12 @@
     extraPackages = with pkgs; [ adwaita-icon-theme];
     extraCompatPackages = with pkgs; [ proton-ge-bin proton-cachyos_x86_64_v3 ];
   };
-
+  programs = {
+  gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
+  };
   services.switcherooControl.enable = true;
   services.udev.packages = [ pkgs.switcheroo-control ];
 
