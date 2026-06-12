b12ff52395753057e2afa3af0efb7378ebc4a4b3
ChaoticChrono <ChaoticChrono@proton.me>
2026-06-12 18:39:15 +0530

    Flake Update

--- configuration.nix
+++ configuration.nix
@@ -20,7 +20,7 @@
     options kvm_intel nested=1
     options kvm_intel emulate_invalid_guest_state=0
     options kvm ignore_msrs=1
-  '';  
+  '';
   # Lanzaboote handles systemd-boot overrides nativly
   boot.loader.systemd-boot.enable = lib.mkForce false;
   boot.loader.efi.canTouchEfiVariables = true;
