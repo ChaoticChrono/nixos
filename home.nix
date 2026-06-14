545f5b61f9dd9228067fb2d80ae24e3ea9213b15
ChaoticChrono <ChaoticChrono@proton.me>
2026-06-14 09:49:40 +0530

    added anime game launcher

--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@
   home.stateVersion = "26.05";
   home.sessionVariables = {
    GAMEMODERUNEXEC="env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only";
+   TERMINAL = "ghostty";
    };
   # Universal cursor settings
   home.pointerCursor = {
