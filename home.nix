1af807f9b726e6e57587f7451907b2bb65e0672d
ChaoticChrono <ChaoticChrono@proton.me>
2026-06-16 18:48:35 +0530

    waybar

--- home.nix
+++ home.nix
@@ -110,8 +110,8 @@ programs.waybar = {
 
     "hyprland/workspaces" = {
       format = "{icon}";
-     on-scroll-up =  "hyprctl dispatch workspace e+1";
-     on-scroll-down = "hyprctl dispatch workspace e-1";
+     on-scroll-up =  "hyprctl dispatch workspace +1";
+     on-scroll-down = "hyprctl dispatch workspace -1";
     };
 
     "hyprland/window" = {
