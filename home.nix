b89d2dea923b46316401502745e82ebb40d4340c
ChaoticChrono <ChaoticChrono@proton.me>
2026-06-16 14:52:48 +0530

    fixed theme

--- home.nix
+++ home.nix
@@ -8,12 +8,34 @@
   home.sessionVariables = {
    GAMEMODERUNEXEC="env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only";
    };
+   home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+
+    package = pkgs.adwaita-icon-theme;
+    name = "Adwaita";
+    size = 32;
+  };
+   dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      accent-color = "blue";
+    };
+  };
+   gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.adw-gtk3;
+      name = "adw-gtk3-dark";
+    };
+    iconTheme = {
+      package = pkgs.morewaita-icon-theme;
+      name = "MoreWaita";
+    };
+   };
   qt = {
     enable = true;
-  };
-  wayland.windowManager.sway = {
-  enable = true;
-  wrapperFeatures.gtk = true;
+    platformTheme.name = "gtk";
   };
 programs.firefox = {
     enable = true;
@@ -39,13 +61,13 @@ programs.firefox = {
    programs.fastfetch.enable = true;
    programs.kitty.enable = true;
    wayland.windowManager.hyprland.systemd.enable = false;
-programs.git = {
-  enable = true;
-  settings = {
-  user = {
-      name = "Vedanta Singh";
-      email = "ChaoticChrono@proton.me";
-    };
+   programs.git = {
+     enable = true;
+     settings = {
+       user = {
+         name = "Vedanta Singh";
+         email = "ChaoticChrono@proton.me";
+               };
   init.defaultBranch = "main";
   alias = {
       ac = "!f() { git add . && git commit -m \"$1\"; }; f";
