432753680653a47164b6dc759db0feb245868907
ChaoticChrono <ChaoticChrono@proton.me>
2026-06-17 19:33:47 +0530

    added nvidia offload by default

--- home.nix
+++ home.nix
@@ -7,6 +7,10 @@
   home.homeDirectory = "/home/ved";
   home.stateVersion = "26.05";
   home.sessionVariables = {
+   __NV_PRIME_RENDER_OFFLOAD="1";
+   __NV_PRIME_RENDER_OFFLOAD_PROVIDER="NVIDIA-G0";
+   __GLX_VENDOR_LIBRARY_NAME="nvidia";
+   __VK_LAYER_NV_optimus="NVIDIA_only";
    GAMEMODERUNEXEC="env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia __VK_LAYER_NV_optimus=NVIDIA_only";
    };
    home.pointerCursor = {
