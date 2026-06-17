eb7d0de99d0682f6a34cb68a2e26b43d42155b7b
ChaoticChrono <ChaoticChrono@proton.me>
2026-06-17 11:40:58 +0530

    scaling

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -21,8 +21,8 @@ hl.monitor({
     position = "auto",
     scale    = "auto",
 })
-hl.monitor({ output = "eDP-1", mode = "1920x1080", position = "1280x0", scale = 1.5, bitdepth = 10 })
-hl.monitor({ output = "HDMI-A-1", mode = "1920x1080", position = "0x0", scale = 1.5 })
+hl.monitor({ output = "eDP-1", mode = "1920x1080", position = "1422x0", scale = 1.35, bitdepth = 10 })
+hl.monitor({ output = "HDMI-A-1", mode = "1920x1080", position = "0x0", scale = 1.35 })
 -- unscale XWayland
 hl.config({
   xwayland = {
