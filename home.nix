3e6025aa024077587455d51f80e3d92fb864f345
ChaoticChrono <ChaoticChrono@proton.me>
2026-06-19 10:10:32 +0530

    Hypr

--- home.nix
+++ home.nix
@@ -394,7 +394,6 @@
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
     # make sure to also set the portal package, so that they are in sync
     portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-    withUWSM = true;
     xwayland.enable = true;
     systemd.enable = false;
   };
